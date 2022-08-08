e5c977212f9fcaa7857fbe5c7711cbc889348ae3
SoumyajeetDas <soumyajeetdas1998@gmail.com>
2022-08-08 23:35:47 +0530

    Enabling CORS

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const app = express();
 const contentRouter = require('./routes/contentRouter')
+const cors = require('cors')
 
 
 
+app.use(cors());
 app.use("/api/v1/fops/contents",contentRouter)
 
 
--- routes/contentRouter.js
+++ routes/contentRouter.js
@@ -4,6 +4,8 @@ const triageController = require('../controllers/triageController')
 const router = express.Router();
 
 
+
+// Route for Traige
 router
 .route("/triage")
 .get(triageController.getAllTriage);
