737a29f89eb6543e69bd844fcc11c832cc981344
SoumyajeetDas <soumyajeetdas1998@gmail.com>
2022-08-14 10:28:40 +0530

    Added 404 Router

--- app.js
+++ app.js
@@ -9,5 +9,13 @@ app.use(cors());
 app.use("/api/v1/fops/contents",contentRouter)
 
 
+app.use("*",(req,res)=>{
+    res.status(404).send({
+        status : "404 Error",
+        message:"Url not present"
+    })
+})
+
+
 
 module.exports = app;
