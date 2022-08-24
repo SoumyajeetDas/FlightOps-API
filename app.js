21e05bd9bf7d19ec85d9c7f62900fae5f9ea9322
SoumyajeetDas <soumyajeetdas1998@gmail.com>
2022-08-24 22:14:59 +0530

    Change in CORS

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ app.use(cors({
     methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
 }));
 
+app.options('*',cors());
+
 // app.options('*',cors({
 //     origin: ['https://main--bejewelled-longma-891e99.netlify.app', 'http://localhost:3000']
 // })); 
