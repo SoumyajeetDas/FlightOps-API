c3a2f2654eedfab1e80ad68ff152fe36b5098d3e
SoumyajeetDas <soumyajeetdas1998@gmail.com>
2022-08-27 13:03:59 +0530

    Revert "Again change in CORS"
    This reverts commit 9c1aaaacb3d02365b0cde009ce084550561b1d53.

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ app.use(cors({
     // methods: ['GET','POST','DELETE','UPDATE','PUT','PATCH']
 }));
 
-app.options('*',cors());
+// app.options('*',cors());
 
-// app.options('*',cors({
-//     origin: ['https://main--bejewelled-longma-891e99.netlify.app', 'http://localhost:3000']
-// })); 
+app.options('*',cors({
+    origin: ['https://main--bejewelled-longma-891e99.netlify.app', 'http://localhost:3000']
+})); 
 
 
 app.use("/api/v1/fops/contents",contentRouter)
